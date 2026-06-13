af9135cffe4e66320ab2b76af57cbec6eecd31a9
makazis <labdiennabagi@gmail.com>
2026-06-14 01:21:40 +0300

    added basic models and a super user not secure because i felt like it also i did get distracted by a side tangent of venting lore about a good series to a friend of mine, meaning that i lost some time, but oh well. Also did add some models. Might work a bit more on them later.

--- iloveforuming/posts/models.py
+++ iloveforuming/posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Tag(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="posts")
+    prompt=models.CharField(max_length=500) # So, the way i want to do this is the model recognising the words in english from a sentence, maybe turning them into their default forms, and then typing all of them in a list, and chekcing for it. 
+    timestamp=models.DateTimeField(auto_now_add=True)
+    symbols=models.CharField(max_length=500) #Think we will do ASCII printable characters (32-127) for symbol storage
+    def __str__(self):
+        return f"posted by {self.author} at {self.timestamp}"
+class Comment(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name="comments")
+    post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
+    symbols=models.CharField(max_length=500)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"comment by {self.author.username} on post {self.post.id}"
+#username: admin
+#password: admin
