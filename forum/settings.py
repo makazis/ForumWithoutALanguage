a2fb16272829f5591288b4e59b31f7e59e5e021d
makazis <labdiennabagi@gmail.com>
2026-06-13 23:32:43 +0300

    Initial commit
    oooh haha look i am so cooked
    Then again, i am one of the greatest programmers
    i've seen in my life, so this should be fun.

---
+++ forum/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'forum_db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': 'highly_secure_admin_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
